f3cb208e4e0f70b6aa7dda7d1135c56f82cbba67
LBMicheal <bohlmeyerm@gmail.com>
2023-03-05 22:59:23 -0600

    add reset button

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 /* CREATES A GRID OF DIVS */
+var numberOfColumns = 16;
+var numberOfRows = 16;
+const resetButton = document.querySelector(".resetButton");
+
 var grid = document.createElement("div");
 grid.className = "grid";
-for (var i = 0; i < 16; ++i) {
-  var column = document.createElement("div");
-  column.className = "column";
-  for (var j = 0; j < 16; ++j) {
-    var row = document.createElement("div");
-    row.className = "row";
-    column.appendChild(row);
+createGrid();
+
+function createGrid() {
+  for (var i = 0; i < numberOfColumns; ++i) {
+    var column = document.createElement("div");
+    column.className = "column";
+    for (var j = 0; j < numberOfRows; ++j) {
+      var row = document.createElement("div");
+      row.className = "row";
+      column.appendChild(row);
+    }
+    grid.appendChild(column);
   }
-  grid.appendChild(column);
+  document.body.appendChild(grid);
 }
-document.body.appendChild(grid);
 
 /* ACTIVATE THE GRID AS THE MOUSE HOVERS OVER IT */
 grid.addEventListener("mouseover", function (e) {
@@ -19,3 +27,15 @@ grid.addEventListener("mouseover", function (e) {
     e.target.classList.add("trail");
   }
 });
+
+/* BUTTON FOR RESETTING GRID  */
+resetButton.addEventListener("click", gridReset);
+
+/* FUNCTION FOR RESETTING GRID */
+function gridReset() {
+  let number = prompt("Please enter number of squares per side? (up to 100)");
+  numberOfColumns = number;
+  numberOfRows = number;
+
+  createGrid();
+}
